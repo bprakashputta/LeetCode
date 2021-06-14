c36020379ae33e460fb03811a93c22a6c981fa03
bprakashputta <bhanu.putta1999@gmail.com>
2021-06-14 20:31:11 +0530

    Square Root of x

--- Math/Math.cpp
+++ Math/Math.cpp
@@ -44,6 +44,26 @@ bool isPalindrome(int num) {
 return rev == num;
 }
 
+//Find square root of x 
+// using binary search
+int mySqrt(int x) {
+    if(x==0||x==1) return x;
+    int temp = x/2;
+    long long l=0;
+    long long r=x/2;
+    long long mid = 0, ans=0;
+    while(l<=r){
+        mid = l + (r-l)/2;
+        if(mid*mid<=x){
+            ans = max(ans, mid);
+            l = mid+1;
+        }else{
+            r = mid -1;
+        }
+    }
+return ans;
+}
+
 int main(){
     
 }
