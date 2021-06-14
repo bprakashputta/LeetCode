b04462020c7ca284ddea4e9a0ba1393d5dc7dbef
bprakashputta <bhanu.putta1999@gmail.com>
2021-06-14 19:51:23 +0530

    Added Reverse2

--- Math/Math.cpp
+++ Math/Math.cpp
@@ -13,6 +13,26 @@ int reverse(int x) {
 return rev;
 }
 
+int reverse2(int num){
+    bool isp = false;
+    long x = num;
+    if(x<0){
+        isp = true;
+        x = x*(-1);
+    }
+    long rev = 0;
+    while(x>0){
+        rev = rev*10 + (x%10);
+        x = x/10;
+    }
+    if(isp==true)rev=rev*(-1);
+    if(rev>pow(2,31)-1 || rev<(-1)*pow(2,31))
+        return 0;
+    else
+        return rev;
+  
+}
+
 int main(){
     
 }
